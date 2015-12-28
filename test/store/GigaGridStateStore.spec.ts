69c5ba79ec274254874a442249a1bd824a35fe0c
Erfang Chen <erfangc@gmail.com>
2015-12-27 22:25:07 -0500

    hooked up more flux things, added more actions

--- test/store/GigaGridStateStore.spec.ts
+++ test/store/GigaGridStateStore.spec.ts
@@ -4,15 +4,16 @@ import {GigaGridAction} from "../../src/store/GigaGridStateStore";
 import {GigaGridStateStore} from "../../src/store/GigaGridStateStore";
 import {GigaGridProps} from "../../src/components/GigaGrid";
 import {TestUtils} from "../TestUtils";
-import {SubtotalAction} from "../../src/store/GigaGridStateStore";
+import {NewSubtotalAction} from "../../src/store/GigaGridStateStore";
+import {GigaGridActionType} from "../../src/store/GigaGridStateStore";
 
 describe("GigaGridStateStore", ()=> {
 
     var dispatcher:Dispatcher<GigaGridAction>;
     var store:GigaGridStateStore;
     var props:GigaGridProps;
-    var subtotalAction:SubtotalAction = {
-        type: "subtotal",
+    var newSubtotalAction:NewSubtotalAction = {
+        type: GigaGridActionType.NEW_SUBTOTAL,
         subtotalBys: [{colTag: "gender"}]
     };
 
@@ -25,7 +26,7 @@ describe("GigaGridStateStore", ()=> {
         store = new GigaGridStateStore(dispatcher, props);
     });
 
-    it("deduces the correct initial state", ()=> {
+    it("can deduce the correct initial state", ()=> {
         const initialState = store.getInitialState();
         expect(initialState).toBeTruthy();
         expect(initialState.subtotalBys).toBeFalsy();
@@ -35,10 +36,10 @@ describe("GigaGridStateStore", ()=> {
         expect(state.tree.getRoot().detailRows.length).toBe(10);
     });
 
-    it("can handle subtotal action", ()=> {
-        dispatcher.dispatch(subtotalAction);
+    it("can handle new subtotal action", ()=> {
+        dispatcher.dispatch(newSubtotalAction);
         expect(store.getState().subtotalBys.length).toBe(1);
-        expect(store.getState().subtotalBys[0]).toBe(subtotalAction.subtotalBys[0]);
+        expect(store.getState().subtotalBys[0]).toBe(newSubtotalAction.subtotalBys[0]);
         const children = store.getState().tree.getRoot().getChildren();
         expect(children.length).toBe(2);
         expect(store.getState().tree.getRoot().getChildByTitle("Male")).toBeDefined();
