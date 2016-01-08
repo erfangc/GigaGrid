2c81799aaf9fffc25f8e57627ffd23063cda868f
Erfang Chen <erfangc@gmail.com>
2016-01-08 16:07:02 -0500

    consolidated examples and prettied it

--- examples/basic_example.ts
+++ examples/basic_example.ts
@@ -9,11 +9,10 @@ import {ColumnFormat} from "../src/models/ColumnLike";
 
 const element = React.createElement(GigaGrid, {
     bodyHeight: "250px",
-    bodyWidth: "800px",
     data: TestUtils.getSampleData().data,
     columnDefs: TestUtils.getSampleData().columnDefs,
     initialSubtotalBys: [{colTag: "gender"}],
     initialSortBys: [{colTag: "gift", format: ColumnFormat.NUMBER, direction: SortDirection.ASC}]
 });
 
-ReactDOM.render(element, document.getElementById('giga-grid-container'));
+ReactDOM.render(element, document.getElementById('basic-example'));
---
+++ examples/custom_cell_template.ts
@@ -0,0 +1,26 @@
+import {GigaGrid} from "../src/components/GigaGrid";
+import {TestUtils} from "../test/TestUtils";
+import {SubtotalBy} from "../src/models/ColumnLike";
+import * as $ from 'jquery';
+import React = require('react');
+import ReactDOM = require('react-dom');
+import {SortDirection} from "../src/models/ColumnLike";
+import {ColumnFormat} from "../src/models/ColumnLike";
+import {TableRowColumnDef} from "../src/models/ColumnLike";
+
+const columnDefs = TestUtils.getSampleData().columnDefs;
+const customColumnDef = columnDefs[0];
+
+customColumnDef.cellTemplateCreator = (data:any) => {
+    return React.createElement("a", {className: "special-cell", onClick: ()=>{alert(`You clicked on ${data}`)}}, data);
+};
+
+const element = React.createElement(GigaGrid, {
+    bodyHeight: "250px",
+    data: TestUtils.getSampleData().data,
+    columnDefs: columnDefs,
+    initialSubtotalBys: [{colTag: "gender"}],
+    initialSortBys: [{colTag: "gift", format: ColumnFormat.NUMBER, direction: SortDirection.ASC}]
+});
+
+ReactDOM.render(element, document.getElementById('custom-cell-template'));
