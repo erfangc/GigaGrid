e3ccc1f8ebcca68449cf933054fddb19828eb97b
Erfang Chen <erfangc@gmail.com>
2016-01-08 23:40:43 -0500

    Handle Cell/Row select #5

--- examples/examples.ts
+++ examples/examples.ts
@@ -1,12 +1,13 @@
 import './basic_example';
 import './custom_cell_template';
+import './row_selection';
 
 import * as $ from 'jquery';
 import * as _ from 'lodash';
 
 declare var hljs;
 
-$('pre code').each(function(i, block) {
+$('pre code').each(function (i, block) {
     hljs.highlightBlock(block);
 });
 
