c5daa4746194fb37a0d1ea4280f353e16e384215
Erfang Chen <erfangc@gmail.com>
2016-01-10 22:13:08 -0500

    Create a Module System for Loading Source Code into the Browser for DEV #2
    webpack config now working, but font-awesome still must be manually included b/c of binary dependencies that cannot be bundled somehow, need additional research

---
+++ index.ts
@@ -0,0 +1,9 @@
+import "./src/styles/giga-grid.css";
+import "./jspm_packages/npm/font-awesome@4.5.0/css/font-awesome.css";
+import {GigaGrid as GG} from "./src/components/GigaGrid";
+import {ColumnFormat as CF} from "./src/models/ColumnLike";
+import {AggregationMethod as AM} from "./src/models/ColumnLike";
+
+export var GigaGrid = GG;
+export var ColumnFormat = CF;
+export var AggregationMethod = AM;
