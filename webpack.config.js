63444589afe3aacaba34bc92e631f3f388b548d8
Erfang Chen <erfangc@gmail.com>
2016-01-10 22:13:08 -0500

    Create a Module System for Loading Source Code into the Browser for DEV #2
    webpack config now working, but font-awesome still must be manually included b/c of binary dependencies that cannot be bundled somehow, need additional research

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,47 @@
-// todo use a different entry point that targets the browser and make global exports available
+// todo fix css loader, it not resolving giga-grid.css
 module.exports = {
-    entry: "./src/components/GigaGrid.js",
+    entry: "./index.js",
     output: {
         path: "./dist",
+        libraryTarget: "umd",
+        library: "GigaGrid",
         filename: "giga-grid.js"
     },
     module: {
         loaders: [
-            {test: /\.css$/, loader: "style!css"}
+            {
+                test: /\.css$/, loader: "style-loader!css-loader"
+            },
+            {
+                test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/font-woff"
+            }, {
+                test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/font-woff"
+            }, {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/octet-stream"
+            }, {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "file"
+            }, {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=image/svg+xml"
+            }
         ]
+    },
+    externals: {
+        "react": {
+            commonjs: 'react',
+            commonjs2: 'react',
+            amd: 'react',
+            root: 'React'
+        },
+        "react-dom": {
+            commonjs: 'react-dom',
+            commonjs2: 'react-dom',
+            amd: 'react-dom',
+            root: 'ReactDOM'
+        }
     }
 };
