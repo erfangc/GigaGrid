55b7d1e4c4d21e73a74694dec601ca1188e7d9fc
Erfang Chen <erfangc@gmail.com>
2016-02-25 21:19:18 -0500

    minor fixes after publish

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import "../../jspm_packages/npm/font-awesome@4.5.0/css/font-awesome.css";
 import {GigaGrid as GG} from "./components/GigaGrid";
 import {ColumnFormat as CF} from "./models/ColumnLike";
 import {AggregationMethod as AM} from "./models/ColumnLike";
+import {SortDirection as SD} from "./models/ColumnLike";
 
 export var GigaGrid = GG;
 export var ColumnFormat = CF;
 export var AggregationMethod = AM;
+export var SortDirection = SD;
