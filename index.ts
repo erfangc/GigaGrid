5de6ace4217f9f86bf61c9dc8f585fe00c86fb62
Christopher Guthrie <christopher.guthrie@blackrock.com>
2016-05-27 11:08:55 -0400

    Adding export of format function

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 export {GigaGrid} from "./src/components/GigaGrid";
 export {AggregationMethod, ColumnDef, ColumnFormat, FormatInstruction, SortDirection} from './src/models/ColumnLike';
 export {DefaultCellRenderer, CellProps} from "./src/components/Cell.tsx";
+export {format} from "./src/static/SubtotalAggregator";
