63aef6bfa2e50b903abdccb36df68a061558b169
Erfang Chen <erfangc@gmail.com>
2016-10-30 13:07:52 -0400

    reformatting and style changes

--- src/components/TableHeader.tsx
+++ src/components/TableHeader.tsx
@@ -5,9 +5,9 @@ import {GridComponentProps, getScrollBarWidth} from "./GigaGrid";
 import Dispatcher = Flux.Dispatcher;
 
 export interface TableHeaderProps extends GridComponentProps<TableHeader> {
-    tableHeaderClass?:string
-    columns:Column[][]
-    staticLeftHeaders:number
+    tableHeaderClass?: string
+    columns: Column[][]
+    staticLeftHeaders: number
 }
 
 /**
@@ -18,7 +18,7 @@ export interface TableHeaderProps extends GridComponentProps<TableHeader> {
  */
 export class TableHeader extends React.Component<TableHeaderProps,any> {
 
-    constructor(props:TableHeaderProps) {
+    constructor(props: TableHeaderProps) {
         super(props);
     }
 
@@ -30,43 +30,50 @@ export class TableHeader extends React.Component<TableHeaderProps,any> {
         )
     }
 
-    private renderHeaderRows():JSX.Element[] {
-        const trs:JSX.Element[] = [];
-        var i:number;
+    private renderHeaderRows(): JSX.Element[] {
+        const trs: JSX.Element[] = [];
+        var i: number;
         for (i = 0; i < this.props.columns.length - 1; i++)
             trs.push(TableHeader.renderColumnGroups(this.props.columns[i], i));
         trs.push(this.renderLeafColumns(this.props.columns[i], i));
         return trs;
     }
 
-    private static renderColumnGroups(columns:Column[], key:number):JSX.Element {
-        const ths = columns.map((column:Column, i:number)=> {
-            const style:any = {
+    private static renderColumnGroups(columns: Column[], key: number): JSX.Element {
+        const ths = columns.map((column: Column, i: number)=> {
+            const style: any = {
                 width: column.colSpan + "px"
             };
             return (
-                <div className="column-group" key={i} style={style}><span className="content header-text">{column.title}</span></div>
+                <div className="column-group" key={i} style={style}>
+                    <span className="content header-text">{column.title}</span>
+                </div>
             );
         });
         return (<div className="column-group-row" key={key}>{ths}</div>);
     }
 
-    private renderLeafColumns(columns:Column[], key:number):JSX.Element {
-        const ths = columns.map((column:Column, i:number)=> {
-            return <TableHeaderCell column={column} key={i}
-                                    isFirstColumn={i===0}
-                                    isLastColumn={i===columns.length-1}
-                                    columnNumber={i}
-                                    tableHeaderClass={this.props.tableHeaderClass}
-                                    gridProps={this.props.gridProps}
-                                    dispatcher={this.props.dispatcher}/>
+    private renderLeafColumns(columns: Column[], key: number): JSX.Element {
+        const ths = columns.map((column: Column, i: number)=> {
+            return (
+                <TableHeaderCell column={column}
+                                 key={i}
+                                 isFirstColumn={i===0}
+                                 isLastColumn={i===columns.length-1}
+                                 columnNumber={i}
+                                 tableHeaderClass={this.props.tableHeaderClass}
+                                 gridProps={this.props.gridProps}
+                                 dispatcher={this.props.dispatcher}
+                />
+            );
         });
         // add a placeholder to offset the scrollbar
-        ths.push(<div className="blank-header-cell text-align-right table-header" key={ths.length} style={{minWidth: '0', width:`${getScrollBarWidth()}px`}}>&nbsp;</div>);
-        if( this.props.staticLeftHeaders > 0 ){
+        ths.push(<div className="blank-header-cell text-align-right table-header" key={ths.length}
+                      style={{minWidth: '0', width:`${getScrollBarWidth()}px`}}>&nbsp;</div>);
+        if (this.props.staticLeftHeaders > 0) {
             const leftHeaders = ths.slice(0, this.props.staticLeftHeaders);
             const rightScrollingHeaders = ths.slice(this.props.staticLeftHeaders);
-            return(
+            return (
                 <div key={key}>
                     <div className="left-static-headers">{leftHeaders}</div>
                     <div className="right-scrolling-headers">{rightScrollingHeaders}</div>
