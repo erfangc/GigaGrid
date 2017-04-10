10fa248b66cb57ce590b8d9e364a2144e7475774
Mehul Gupta <mehulgupta29@gmail.com>
2017-04-10 14:19:38 -0400

    adding bracket styling for aggregationMethod COUNT and COUNT_DISTINCT

--- src/components/Cell/CellRenderer.tsx
+++ src/components/Cell/CellRenderer.tsx
@@ -53,7 +53,13 @@ export class CellRenderer {
         let {row, column} = this.props;
         let renderedCellContent: JSX.Element|string|number = format(row.get(column), column.formatInstruction) || "";
         if (!row.isDetailRow()
-            && (column.aggregationMethod === AggregationMethod.COUNT || column.aggregationMethod === AggregationMethod.COUNT_DISTINCT))
+            && (/* If aggregationMethod is of type Number | AggregationMethod.key e.g. 0,1,2,.. */
+                AggregationMethod.COUNT === column.aggregationMethod ||
+                AggregationMethod.COUNT_DISTINCT === column.aggregationMethod ||
+                /* If aggregationMethod is of type string e.g. "COUNT", "COUNT_DISTINCT",... */
+                AggregationMethod.COUNT === Number(AggregationMethod[column.aggregationMethod]) ||
+                AggregationMethod.COUNT_DISTINCT === Number(AggregationMethod[column.aggregationMethod])
+            ))
             renderedCellContent = `[${renderedCellContent}]`;
         return this.renderContentContainerWithElement(
             <span className="content">
